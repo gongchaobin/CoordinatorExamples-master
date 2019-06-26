c3a205226dceac542a16e8994d2a17e611fe2584
gongChaobin <gongchaobin@promote.cache-dns.local>
2019-06-26 18:58:10 +0800

    修改gradle配置

--- app/src/main/java/saulmm/coordinatorexamples/RxJavaTestActivity.java
+++ app/src/main/java/saulmm/coordinatorexamples/RxJavaTestActivity.java
@@ -3,7 +3,6 @@ package saulmm.coordinatorexamples;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,7 +15,6 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -146,16 +144,15 @@ public class RxJavaTestActivity extends AppCompatActivity{
 
 
 
-
-
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-            @Override
-            public void log(String message) {
-                Log.i(TAG,"message: " + message);
-            }
-        });
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        httpClientBuilder.addInterceptor(interceptor);
+//
+//        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+//            @Override
+//            public void log(String message) {
+//                Log.i(TAG,"message: " + message);
+//            }
+//        });
+//        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//        httpClientBuilder.addInterceptor(interceptor);
 
 
         Retrofit retrofit = new Retrofit.Builder()
