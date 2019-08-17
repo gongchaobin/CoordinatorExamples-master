d6ef8a1fcbd77a168898706bbfcbe339d55fcf95
gongChaobin <984519762@qq.com>
2019-08-17 14:54:44 +0800

    原生调flutter

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,14 +1,13 @@
-import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'package:flutter/material.dart';
 
 void main() => runApp(_widgetForRoute(window.defaultRouteName));
 
 Widget _widgetForRoute(String route) {
-  switch(route) {
-    case "route1":
+  switch (route) {
+    case 'route1':
       return Center(
         child: Text('Unknown route: $route', textDirection: TextDirection.ltr),
       );
   }
-
 }
